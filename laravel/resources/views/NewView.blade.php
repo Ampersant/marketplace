eb0528b1e673f72d9799f730dd9914fc1ca1957d
essayersn <polosatuy.enot@gmail.com>
2024-01-25 15:35:01 +0100

    5

--- laravel/resources/views/NewView.blade.php
+++ laravel/resources/views/NewView.blade.php
@@ -1,3 +1,21 @@
-<div>
-   SLAVA UKRAINE
-</div>
+<template>
+   <p @click="hui">slava ukraini</p>
+</template>
+
+<script>
+   data() {
+      return{
+
+      }
+   },
+   methods: {
+      hui(){
+         alert(1)
+      }
+   }
+</script>
+
+<style lang="sass">
+
+</style>
+
