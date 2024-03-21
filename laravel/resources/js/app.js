97378e1851e2280147eba529a8bcb31c26ec9be2
Ampersant <pika4yxa1337@gmail.com>
2024-03-21 19:37:27 +0100

    Update app.js

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,12 +1,11 @@
 import './bootstrap';
 import { createApp } from 'vue/dist/vue.esm-bundler.js'
 import MarketplaceMain from './components/MarketplaceMain.vue'
-import AutorisationPage from './components/AutorisationPage.vue'
-import ProfileMain from './components/profile/ProfileMain.vue'
+
 
 const app = createApp({
    components: {
-      MarketplaceMain, AutorisationPage, ProfileMain,
+      MarketplaceMain
    }
 })
 app.mount('#app')
